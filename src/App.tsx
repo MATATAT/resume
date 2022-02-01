be0269ae8b7d8e7a848f96a6302be7e1fa0e463f
Matt Macdonald <matt.david.macdonald@gmail.com>
2022-02-01 12:26:14 -0800

    removing more material

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,25 @@ import Header from './Header';
 
 import SideBar from './SideBar';
 import MainExperience from './MainExperience';
-import { Grid, Box } from '@mui/material';
 
 import './App.css'
 
 export default function App(props: Resume) {
     return (
-        <Box className='App'>
+        <div className='App'>
             <Header
                 name={props.name} 
                 title={props.title} />
-            <Grid container spacing={3}>
+            <div className='App-main'>
+                <SideBar 
+                    contact={props.contact} 
+                    education={props.education} 
+                    qualifications={props.qualifications} />
+                <MainExperience 
+                    experience={props.experience} />
+            </div>
+                
+            {/* <Grid container spacing={3}>
                 <Grid item xs={3.5}>
                     <SideBar 
                         contact={props.contact} 
@@ -24,7 +32,7 @@ export default function App(props: Resume) {
                     <MainExperience 
                         experience={props.experience} />
                 </Grid>
-            </Grid>
-        </Box>
+            </Grid> */}
+        </div>
     );
 }
