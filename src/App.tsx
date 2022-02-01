948121138e60eb9a7f4c5dc457c335d7deaae1dc
Matt Macdonald <matt.david.macdonald@gmail.com>
2022-02-01 12:26:45 -0800

    Forgot to remove the commented code

--- src/App.tsx
+++ src/App.tsx
@@ -20,19 +20,6 @@ export default function App(props: Resume) {
                 <MainExperience 
                     experience={props.experience} />
             </div>
-                
-            {/* <Grid container spacing={3}>
-                <Grid item xs={3.5}>
-                    <SideBar 
-                        contact={props.contact} 
-                        education={props.education} 
-                        qualifications={props.qualifications} />
-                </Grid>
-                <Grid item xs={6}>
-                    <MainExperience 
-                        experience={props.experience} />
-                </Grid>
-            </Grid> */}
         </div>
     );
 }
